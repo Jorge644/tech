2c62411bb844051b16bccc7cf4972c32c3a56ea2
Jorge Castro <castrojorgeml@gmail.com>
2021-02-26 12:11:52 -0300

    Adding project

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    plugins: ['@babel/plugin-transform-async-to-generator'],
+    presets: [
+      [
+        '@babel/env',
+        {
+          targets: {
+            node: 'current'
+          },
+          modules: 'cjs'
+        }
+      ]
+    ]
+  }
